88679e650cc0aa83ec5a7177bd828af617e43796
hgarciaospina <31645964+hgarciaospina@users.noreply.github.com>
2018-08-11 22:53:28 -0500

    Initial commit

---
+++ Sistem_Ventas/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Sistem_Ventas.Models;
+
+namespace Sistem_Ventas.Controllers
+{
+    public class HomeController : Controller
+    {
+        IServiceProvider serviceProvider;
+        public HomeController(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        public async Task<IActionResult> Index()
+        {
+            await CreateRoles(serviceProvider);
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            String[] rolesName = { "Admin", "User" };
+            foreach (var item in rolesName)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(item);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(item));
+                }
+            }
+            var user = await userManager.FindByIdAsync("c400982d-834e-4c03-a0a4-94795f98ece1");
+            await userManager.AddToRoleAsync(user, "Admin");
+        }
+    }
+
+}
