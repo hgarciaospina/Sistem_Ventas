e537c15124a43129bc4b2a84816830ac874af303
hgarciaospina <31645964+hgarciaospina@users.noreply.github.com>
2018-08-15 14:13:32 -0500

    Sección 3, clase 15
    Empezando  a crear los procedimientos para verficar la información del usuario

---
+++ Sistem_Ventas/Library/ListObject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Sistem_Ventas.Data;
+using Sistem_Ventas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistem_Ventas.Library
+{
+    public class ListObject
+    {
+        public UsersRoles _usersRole;
+        public UserData _userData;
+        public IdentityError _identityError;
+        public ApplicationDbContext _context;
+
+        public List<SelectListItem> _userRoles;
+
+        public RoleManager <IdentityRole> _roleManager;
+        public UserManager<IdentityUser> _userManager;
+        public SignInManager<IdentityUser> _signInManager;
+
+        public List<object[]> dataList = new List<object[]>();
+
+    }
+}
---
+++ Sistem_Ventas/Library/Usuarios.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistem_Ventas.Library
+{
+    public class Usuarios : ListObject
+    {
+        public Usuarios() {
+
+        }
+
+
+        public Usuarios(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _usersRole = new UsersRoles();
+        }
+
+        public Usuarios(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser>  signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _usersRole = new UsersRoles();
+        }
+
+    }
+}
